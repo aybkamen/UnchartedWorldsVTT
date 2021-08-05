a080dd5209403ce23908754974020ec1888ae6bf
David Socias <david.socias@ocado.com>
2021-08-05 15:46:09 +0100

    from boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const UNCHARTED = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ UNCHARTED.abilities = {
+  "str": "UNCHARTED.AbilityStr",
+  "dex": "UNCHARTED.AbilityDex",
+  "con": "UNCHARTED.AbilityCon",
+  "int": "UNCHARTED.AbilityInt",
+  "wis": "UNCHARTED.AbilityWis",
+  "cha": "UNCHARTED.AbilityCha"
+};
+
+UNCHARTED.abilityAbbreviations = {
+  "str": "UNCHARTED.AbilityStrAbbr",
+  "dex": "UNCHARTED.AbilityDexAbbr",
+  "con": "UNCHARTED.AbilityConAbbr",
+  "int": "UNCHARTED.AbilityIntAbbr",
+  "wis": "UNCHARTED.AbilityWisAbbr",
+  "cha": "UNCHARTED.AbilityChaAbbr"
+};
